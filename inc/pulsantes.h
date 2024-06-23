bda624ac95d0a8a7a78467c3d04658d26c9aeee5
AndresPard0 <carlosandrespardocastro@gmail.com>
2024-06-23 15:24:24 -0500

    Implementacion de maquina de estados finitos

--- inc/pulsantes.h
+++ inc/pulsantes.h
@@ -1,5 +1,22 @@
+// botones.h
+#ifndef PULSANTES_H
+#define PULSANTES_H
+typedef enum {
+    buttonUp,
+    buttonDown,
+    buttonFalling,
+    buttonRising
+} estadosBoton_t;
 
+// Prototipos de funciones
+void mefIniciarBoton();
+void mefActualizarBoton(int16_t button, int16_t i, bool *botones);
+int delayNoBloqueante(int16_t tiempo);
+void botonPrecionado(int16_t j, bool *botones);
+void botonLiberado();
 void BotonesComoEntradaPullUp();
 bool LeerBotones (bool *botones);
 void SentidoDeSecuencia(bool *botones, int *contador, uint8_t ultimoLed);
 void VelocidadDeSecuencia(bool *botones, int *interval);
+
+#endif
--- src/pulsantes.c
+++ src/pulsantes.c
@@ -1,7 +1,10 @@
 #include <Arduino.h>
 #include <stdbool.h>
 #include "../inc/hal.h"
-
+#include "../inc/pulsantes.h"
+static bool botonesAnteriores[4] = {true, true, true, true};  
+unsigned long tiempoInicial = 0;
+estadosBoton_t estadosBoton[4];
 void BotonesComoEntradaPullUp(){
     pinMode(boton1, INPUT_PULLUP);
     pinMode(boton2, INPUT_PULLUP);
@@ -10,36 +13,19 @@ void BotonesComoEntradaPullUp(){
  };
 
 bool LeerBotones (bool *botones){
-    static bool botonesAnteriores[4] = {true, true, true, true};  
     const int pulsante [4] = {boton1, boton2, boton3, boton4};
     for (int i = 0; i < 4; i++) {
-        bool estadoActual = digitalRead(pulsante[i]);
-        // Verificar si el estado actual es diferente al estado anterior
-        if (estadoActual==LOW && botonesAnteriores[i]==HIGH) {
-            //Para evitar problemas con doble boton precionado se toma en cuenta
-            //si uno de los botones presionado ya se encontraba en true
-            if (i==0 && botones[i] != true){
-                botones[i] = !botones[i];
-                botones[i+1] = !botones[i+1];
-            }
-            if (i==1 && botones[i] != true){
-                botones[i-1] = !botones[i-1];
-                botones[i] = !botones[i];
-            }
-            if (i==2 && botones[i] != true){
-                botones[i] = !botones[i];
-                botones[i+1] = !botones[i+1];
-            }
-            if (i==3 && botones[i] != true){
-                botones[i-1] = !botones[i-1];
-                botones[i] = !botones[i];
-            }            
+        mefActualizarBoton(pulsante[i],i,botones);
+    //     bool estadoActual = digitalRead(pulsante[i]);
+    //     // Verificar si el estado actual es diferente al estado anterior
+    //     if (estadoActual==LOW && botonesAnteriores[i]==HIGH) {
+    //         //Para evitar problemas con doble boton precionado se toma en cuenta
+    //         //si uno de los botones presionado ya se encontraba en true
 
+
+    //     //Actualizar el estado anterior del botón
+    //     botonesAnteriores[i] = estadoActual;
         }
-
-        //Actualizar el estado anterior del botón
-        botonesAnteriores[i] = estadoActual;
-    }
 
 }
 
@@ -64,3 +50,87 @@ void VelocidadDeSecuencia(bool *botones, int *interval){
 }
 
 
+void mefIniciarBoton() {
+    estadosBoton[0] = buttonUp;
+    estadosBoton[1] = buttonUp;
+    estadosBoton[2] = buttonUp;
+    estadosBoton[3] = buttonUp;
+}
+
+void botonPrecionado(int16_t j, bool *botones) {
+    digitalWrite(13, HIGH);
+            if (j==0 && botones[j] != true){
+                botones[j] = !botones[j];
+                botones[j+1] = !botones[j+1];
+            }
+            if (j==1 && botones[j] != true){
+                botones[j-1] = !botones[j-1];
+                botones[j] = !botones[j];
+            }
+            if (j==2 && botones[j] != true){
+                botones[j] = !botones[j];
+                botones[j+1] = !botones[j+1];
+            }
+            if (j==3 && botones[j] != true){
+                botones[j-1] = !botones[j-1];
+                botones[j] = !botones[j];
+            }            
+}
+
+void botonLiberado() {
+    digitalWrite(13, LOW);
+}
+
+int delayNoBloqueante(int16_t tiempo) {
+    // Obtener el tiempo actual
+    unsigned long tiempoActual = millis();
+    // Verificar si el tiempo transcurrido es mayor o igual al tiempo especificado
+    if (tiempoActual - tiempoInicial >= tiempo) {
+        tiempoInicial = tiempoActual;
+        return 1;  // Han pasado al menos 'tiempo' milisegundos
+    } else {
+        return 0; // Aún no han pasado 'tiempo' milisegundos
+    }
+}
+
+void mefActualizarBoton(int16_t button, int16_t i, bool *botones) {
+    switch (estadosBoton[i]) {
+    case buttonUp:
+        if (!digitalRead(button)) {
+            estadosBoton[i] = buttonFalling;
+        }
+        break;
+
+    case buttonDown:
+        if (digitalRead(button)) {
+            estadosBoton[i] = buttonRising;
+        }
+        break;
+
+    case buttonFalling:
+        if (delayNoBloqueante(40)) {
+            if (!digitalRead(button)) {
+                estadosBoton[i] = buttonDown;
+                botonPrecionado(i,botones);
+            } else {
+                estadosBoton[i] = buttonUp;
+            }
+        }
+        break;
+
+    case buttonRising:
+        if (delayNoBloqueante(40)) {
+            if (digitalRead(button)) {
+                estadosBoton[i] = buttonUp;
+                botonLiberado();
+            } else {
+                estadosBoton[i] = buttonDown;
+            }
+        }
+        break;
+
+    default:
+        mefIniciarBoton();
+        break;
+    }
+}
